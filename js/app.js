9ab8a5095bc82d2fba666c05c25288499341e6bf
Francisco Martins <francisco.mtins@gmail.com>
2024-10-28 21:46:10 -0300

    projeto básico javascript, html e css  para treino

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+console.log('Olá, Javascript!')
+
+var userName = 'Francisco Martins'
+
+document.getElementById('user-name').innerHTML = userName
+
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
