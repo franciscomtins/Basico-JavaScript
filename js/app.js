f50b165988cefd5be5c05ede862ca81b1efb6c77
Francisco Martins <francisco.mtins@gmail.com>
2024-10-30 21:50:15 -0300

    BDD e Controle de fluxo

--- js/app.js
+++ js/app.js
@@ -55,3 +55,43 @@ document.getElementById('user-name').innerHTML = userName
 // }
 // var resultado = soma(5,5)
 // console.log(resultado)
+
+// Controle de Fluxos //
+
+// sendo um cliente correntista do banco 
+// posso sacar dinheiro em caixas eletrônicos 
+// para poder comprar em lugares que não aceitam o cartão de débito ou crédito
+
+var saldo = 1000
+
+function saque (valor) {
+    if (valor > saldo){
+        console.log('Valor do saque superior ao saldo')
+    } else if (valor > 700) {
+        console.log('Valor do saque é superior ao limite por operação')
+    } else{
+        saldo = saldo - valor
+    }
+
+}
+
+saque (701)
+console.log(saldo)
+
+// Cenario 1: Saque com sucesso
+// Dado que meu saldo é de 1000 reais
+// Quando faço um saque de 500 reais 
+// Então o valor do saque deve ser deduzido do meu saldo 
+
+// Cenario 2: Saque com valor superior ao saldo
+// Dado que meu saldo é de 1000 reais
+// Quando faço um saque de 1001 reais 
+// Então não deve deduzir do meu saldo
+// E deve mostrar uma mensagem de alerta informando que o valor é maior que o saldo
+ 
+// Cenario 3: Saque com valor máximo
+// Dado que meu saldo é de 1000 reais
+// E o valor máximo por operação é de 700
+// Quando faço um saque de 701 reais 
+// Então não deve deduzir do meu saldo
+// E deve mostrar uma mensagem de alerta informando que o valor é superior que o máximo permitido por operação
