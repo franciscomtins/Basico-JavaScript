e2f8e5d36fc9cc4ae33c224c4e6fe5b7483caf5b
Francisco Martins <francisco.mtins@gmail.com>
2024-10-30 21:24:59 -0300

    Correção url relativa das imagens, testando operadores e funções.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,52 @@ var userName = 'Francisco Martins'
 document.getElementById('user-name').innerHTML = userName
 
 
+
+// Variáveis //
+
 // var nome = 'Mestre Yoda'
 // var idade = 100
 // var jedi = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
+
+
+// Operadores matemáticos //
+
+// var n1 = 5
+// var n2 = '5'
+
+// var total = n1 + parseInt(n2)
+// console.log(total)
+
+
+// Operadores de comparação //
+//  == Ignora a tipagem
+// === Valida a tipagem
+
+// var v1 = 5
+// var v2 = '5'
+
+// var resultado = v1 !== v2
+// console.log(resultado)
+
+
+// Funções //
+
+// function soma(n1,n2) {
+//     console.log(n1+n2)
+// }
+// soma(5,5)
+
+// function boasVindas(nome) {
+//     alert(nome + ', seja bem-vindo(a)')
+// }
+// boasVindas('Jessica')
+
+// function soma(n1, n2) {
+//     return n1 + n2
+// }
+// var resultado = soma(5,5)
+// console.log(resultado)
