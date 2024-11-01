7cc44f2a85c56a5303812f81fb33ff919cca77e9
Francisco Martins <francisco.mtins@gmail.com>
2024-10-31 22:00:56 -0300

    Melhoria para Index com vue e criação botão like

--- js/app.js
+++ js/app.js
@@ -1,8 +1,58 @@
 
+
+const LIST = [
+    {
+        id: 1,
+        nome: 'Yoda',
+        avatar: 'imagens/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'Luck Skywalker',
+        avatar: 'imagens/luke.png'
+    },
+    {
+        id: 3,
+        nome: 'Princesa Leia',
+        avatar: 'imagens/leia.png'
+    },
+    {
+        id: 4,
+        nome: 'Han Solo',
+        avatar: 'imagens/hansolo.png'
+    },
+    {
+        id: 5,
+        nome: 'Darth Vader',
+        avatar: 'imagens/vader.png'
+    },
+    {
+        id: 6,
+        nome: 'Chewbacca',
+        avatar: 'imagens/chewbacca.png'
+    },
+    {
+        id: 7,
+        nome: 'R2D2',
+        avatar: 'imagens/r2d2.png'
+    },
+    {
+        id: 8,
+        nome: 'C3p0',
+        avatar: 'imagens/c3po.png'
+    }
+]
+
 const App = new Vue({
     el: '#app', 
     data: {
         title: 'Star Wars Lego',
-        userName: 'Jessica'
+        userName: 'Jessica',
+        characters: LIST
+    },
+    methods: {
+        like(userName) {
+            alert(`O personagem ${userName} recebeu um like!`)
+        }
     }    
 })
